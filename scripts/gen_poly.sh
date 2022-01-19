83a9685a56749c474538dec16c9b06bd962fa245
Berke Ates <beates@student.ethz.ch>
2022-01-19 19:54:36 +0100

    Update gen_poly.sh

--- scripts/gen_poly.sh
+++ scripts/gen_poly.sh
@@ -13,5 +13,13 @@ for filename in $(find $poly/tools/mlir-clang/Test/polybench/* -name '*.c' -not 
     $clang $filename -I $incl -S --O0 -o=$path/full/$fname && mlir-opt --lower-affine $path/full/$fname -o=$path/full_lowered/$fname 
 done
 
+for filename in $(find $poly/tools/mlir-clang/Test/polybench/* -name '*.c' -not -path "$poly/tools/mlir-clang/Test/polybench/utilities/*"); do
+    clang=$poly/build/bin/mlir-clang
+    incl=$poly/tools/mlir-clang/Test/polybench/utilities/
+    bname="$(basename $filename .c)"
+    fname="$bname.mlir"
+    $clang $filename -I $incl --O3 -o=$1bin/clang/$fname
+done
+
 echo -ne "\r\e[K"
 echo -e "\u2705 Generation done!"
