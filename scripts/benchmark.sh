0d98ccf95dd2683db8eeed4f7470aeae4f8fd18b
Berke Ates <beates@student.ethz.ch>
2022-01-19 22:59:31 +0100

    Update benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,13 @@
 
-for i in {1..100}
-do
+for cmpName in gcc pluto plutopar polly pollypar
+  for filename in $(find ./bin/$cmpName/* -name '*.out'); do
+      bname="$(basename $filename .out)"
+      fname="./logs/$cmpName/$bname.mlir"
 
-  $1 >> $2
-  echo "Done C run: $i"
-
+       for i in {1..100}
+          do
+            $filename >> $fname
+            echo "Done $cmpName run: $i"
+          done
+  done
 done
