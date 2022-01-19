41b9366a88fda299fb694c5696208e7459639631
Berke Ates <beates@student.ethz.ch>
2022-01-19 13:36:51 +0100

    added bins

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,17 +15,17 @@ poly="./Polygeist"
 $path/scripts/clear.sh $path
 $path/scripts/gen_poly.sh $path $poly
 
-dart $path/scripts/extract_kernel.dart $path
+$path/scripts/bin/extract_kernel.exe $path
 
 $path/scripts/lower.sh $path
-dart $path/scripts/rename_kernel.dart $path
+$path/scripts/bin/rename_kernel.exe $path
 $path/scripts/compile.sh $path
 
 
 $path/scripts/convert.sh $path
 $path/scripts/translate.sh $path
 
-dart $path/scripts/rename_driver.dart $path
+$path/scripts/bin/rename_driver.exe $path
 $path/scripts/compile_renamed.sh $path
 
 $path/scripts/compile_sdfg.sh $path
