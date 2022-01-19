3018fc9017e8a9f2af552e57f60500e1787cb194
Berke Ates <beates@student.ethz.ch>
2022-01-19 22:35:08 +0100

    Update gen_poly.sh

--- scripts/gen_poly.sh
+++ scripts/gen_poly.sh
@@ -14,13 +14,19 @@ for filename in $(find $poly/tools/mlir-clang/Test/polybench/* -name '*.c' -not 
 done
 
 for filename in $(find $poly/tools/mlir-clang/Test/polybench/* -name '*.c' -not -path "$poly/tools/mlir-clang/Test/polybench/utilities/*"); do
-    clang=$poly/build/bin/mlir-clang
+    mlir-clang=$poly/build/bin/mlir-clang
     incl=$poly/tools/mlir-clang/Test/polybench/utilities/
     polyC=$poly/tools/mlir-clang/Test/polybench/utilities/polybench.c
     bname="$(basename $filename .c)"
     fname="$bname.out"
-    $clang $filename $polyC -DPOLYBENCH_TIME -I $incl --O3 -o=$1/bin/clang/$fname
+    $mlir-clang $filename $polyC -DPOLYBENCH_TIME -I $incl --O3 -o=$1/bin/clang/$fname
+    clang -O3 -march=native -mllvm -polly -mllvm -polly-parallel -lgomp -DUSE_MPI=0 -lm $polyC $filename -I $incl -DPOLYBENCH_TIME -o $1/bin/pollypar/$fname
+    clang -mllvm -polly -lgomp -DUSE_MPI=0 -lm $polyC $filename -I $incl -DPOLYBENCH_TIME -o $1/bin/polly/$fname
+    gcc -O3 -march=native $polyC $filename -I $incl -DPOLYBENCH_TIME  -o $1/bin/gcc/$fname
 done
+
+
+
 
 echo -ne "\r\e[K"
 echo -e "\u2705 Generation done!"
